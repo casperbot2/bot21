43dba945aba1249d8317f2519e3c9bcd58ec862f
casperbot2 <aliahmd2233445t@gmail.com>
2019-11-12 15:27:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("حط الاي دي حق الروم اللي يحصل بيها الاسبام هنا")
+let channel =     client.channels.get("643664081873666080")
 setInterval(function() {
-channel.send(`حط الكلام اللي تبغاه هنا`);
+channel.send(`علي وياك علي علي وياك علي علي وياك علي علي وياك علي علي وياك علي علي وياك علي علي وياك علي علي وياك علي علي وياك علي `);
 }, 30)
 })
 
-client.login(process.env.BOT_TOKEN);
+client2.on("ready", () => {
+let channel =     client.channels.get("643664081873666080")
+setInterval(function() {
+channel.send(` علي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك علي`);
+}, 30)
+})
+
+client3.on("ready", () => {
+let channel =     client.channels.get("643664081873666080")
+setInterval(function() {
+channel.send(`علي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك عليعلي وياك علي`);
+}, 30)
+})
+
+client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
+client3.login(process.env.TOKEN3);
